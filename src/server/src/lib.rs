f246d65da6cab349034503135b6274127121f9be
Leonel Morais <leonel.morais@yahoo.com.br>
2023-04-05 11:00:00 -0300

    feat: scaffold server crate with TCP listener

---
+++ src/server/src/lib.rs
@@ -0,0 +1,40 @@
+pub mod auth;
+pub mod error;
+pub mod http_metrics;
+pub mod message;
+pub mod metrics;
+pub mod opcodes;
+pub mod server;
+pub mod tls;
+
+// Re-export commonly used types
+pub use error::ServerError;
+pub use metrics::Metrics;
+pub use opcodes::*;
+
+// Re-export cluster types from cluster crate for backwards compatibility
+pub use cluster::{
+    ClusterConfig,
+    ClusterManager,
+    NodeRole,
+    NodeState,
+    PeerNode,
+    PeerConnection,
+    ReplicationEntry,
+    ReplicationOp,
+    CLUSTER_OP_HEARTBEAT,
+    CLUSTER_OP_HEARTBEAT_ACK,
+    CLUSTER_OP_REPLICATE,
+    CLUSTER_OP_REPLICATE_ACK,
+    CLUSTER_OP_JOIN,
+    CLUSTER_OP_JOIN_ACK,
+    CLUSTER_OP_SYNC_REQUEST,
+    CLUSTER_OP_SYNC_DATA,
+    CLUSTER_OP_SYNC_COMPLETE,
+    CLUSTER_OP_ELECTION,
+    CLUSTER_OP_ELECTION_ACK,
+    CLUSTER_OP_LEADER_ANNOUNCE,
+    replicate_to_peers,
+    serialize_replication_entry,
+    deserialize_replication_entry,
+};
